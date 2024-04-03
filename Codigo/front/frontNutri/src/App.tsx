0320e1ec8af29945f5bedfaa20506795bdb79144
ana.gomes <analuizasg52@gmail.com>
2024-04-03 15:42:48 -0300

    mudanças app

--- Codigo/front/frontNutri/src/App.tsx
+++ Codigo/front/frontNutri/src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react';
-import  SiderComponent from './components/SiderComponent';
+
 import { RoutesComponent } from './Routes';
-import PatientRegistration from './components/PatientRegistration';
-import { PatientsList } from './components/PatientsList';
+
 function App() {
   return (
     
