1a4e513f8048f2c710a781f0be717c5db250a55a
ana.gomes <analuizasg52@gmail.com>
2024-04-03 15:44:41 -0300

    rotas app

--- Codigo/front/frontNutri/src/App.tsx
+++ Codigo/front/frontNutri/src/App.tsx
@@ -4,25 +4,7 @@ import { RoutesComponent } from './Routes';
 function App() {
   return (
     
-      <PatientRegistration initialValues={{
-      name: '',
-      email: '',
-      birthDate: '',
-      occupation: '',
-      goal: '',
-      adress: {
-        zip: '',
-        state: '',
-        city: '',
-        district: '',
-        street: '',
-        country: ''
-      }
-    }} onSubmit={function (values: any): void {
-      throw new Error('Function not implemented.');
-    } } onCancel={function (): void {
-      throw new Error('Function not implemented.');
-    } } />
+     <RoutesComponent />
   
   );
 }
